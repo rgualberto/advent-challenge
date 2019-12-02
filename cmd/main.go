b113f00b09d5d4a803e16615cad59db38dc652d2
Rodrigo Gualberto <itzdrigz@gmail.com>
2019-12-02 11:44:05 -0500

    day 1 puzzle 2

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"github.com/machineq/advent-stuff/day1/puzzle1"
+	// "github.com/machineq/advent-stuff/day1/puzzle1"
+	"github.com/machineq/advent-stuff/day1/puzzle2"
 )
 
 func main() {
-	puzzle1.Run()
+	// puzzle1.Run()
+	puzzle2.Run()
 }
---
+++ day1/puzzle2/puzzle2.go
@@ -0,0 +1,52 @@
+package puzzle2
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+)
+
+// Run starts the program
+func Run() {
+	file, err := os.Open("./day1/puzzle2/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	finalSum := 0
+
+	fmt.Printf("\n****** Scanning and calculating fuel for compounded fuel mass ********\n")
+	for scanner.Scan() {
+		in := scanner.Text()
+		f, err := strconv.ParseFloat(in, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fuel := calculateFuelRequirements(f)
+		finalSum += int(fuel)
+
+		fmt.Printf("\"%s\" -> %v\n", in, fuel)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("\n\nfinal sum: %v\n\n", finalSum)
+}
+
+func calculateFuelRequirements(mass float64) float64 {
+	floatFuel := math.Floor(mass/3) - 2
+
+	if floatFuel <= 0 {
+		return 0
+	}
+
+	return floatFuel + calculateFuelRequirements(floatFuel)
+}
