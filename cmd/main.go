f28aa2c4430c15c4b6f85ff91aea352f31661730
Rodrigo Gualberto <itzdrigz@gmail.com>
2019-12-02 10:52:09 -0500

    day 1 puzzle 1

---
+++ cmd/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/machineq/advent-stuff/day1/puzzle1"
+)
+
+func main() {
+	puzzle1.Run()
+}
---
+++ day1/puzzle1/puzzleOne.go
@@ -0,0 +1,47 @@
+package puzzle1
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+)
+
+// Run starts the program
+func Run() {
+	file, err := os.Open("./day1/puzzle1/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	finalSum := 0
+
+	fmt.Printf("\n****** Scanning and calculating fuel ********\n")
+	for scanner.Scan() {
+		in := scanner.Text()
+		f, err := strconv.ParseFloat(in, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fuel := calculateFuel(f)
+		finalSum += fuel
+
+		fmt.Printf("\"%s\" -> %v\n", in, fuel)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("\n\nfinal sum: %v\n\n", finalSum)
+}
+
+func calculateFuel(mass float64) int {
+	floatFuel := math.Floor(mass/3) - 2
+	return int(floatFuel)
+}
