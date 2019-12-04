f278952348b7704476b6c56cc982efd88c1ed958
Rodrigo Gualberto <itzdrigz@gmail.com>
2019-12-04 14:56:42 -0500

    d3 TraceWirePath logic

--- day03/day03.go
+++ day03/day03.go
@@ -1,7 +1,44 @@
 package day03
 
+import (
+	"strconv"
+	"strings"
+)
+
+// TraceWirePath steps through a coord instruction set and returns a slice of x/y coord pairings
 func TraceWirePath(wirepath string) ([][]int, error) {
-	return [][]int{}, nil
+	directions := strings.Split(wirepath, ",")
+	x, y := 0, 0
+
+	var wirePathCoordinates [][]int
+	for _, d := range directions {
+		dir := d[0:1]
+		steps, err := strconv.Atoi(d[1:])
+		if err != nil {
+			return wirePathCoordinates, err
+		}
+
+		for i := 0; i < steps; i++ {
+			switch dir {
+			case "U":
+				y++
+				break
+			case "D":
+				y--
+				break
+			case "R":
+				x++
+				break
+			case "L":
+				x--
+				break
+			}
+
+			wirePathCoordinates = append(wirePathCoordinates, []int{x, y})
+		}
+	}
+
+	return wirePathCoordinates, nil
 }
 
 func FindIntersectionPoints(wireCoords1 [][]int, wireCoords2 [][]int) ([][]int, error) {
